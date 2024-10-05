e27c1db49a82bb9472bc6764321515ac12d792b1
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-05 22:41:23 +0300

    feat: complete task create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const __dirname = import.meta.dirname;
+
+const folderName = "files";
+const newFileName = "fresh.txt";
+const contentTextFile = "I am fresh and young";
+const customError = "FS operation failed";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, folderName, newFileName);
+
+  try {
+    await fs.stat(filePath);
+    throw new Error(customError);
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw err;
+    }
+
+    await fs.writeFile(filePath, contentTextFile);
+  }
 };
 
 await create();
