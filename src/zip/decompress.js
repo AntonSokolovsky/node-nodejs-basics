28a5b4bba841cb6236096bed70af8bc8d3d7e800
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-06 23:43:27 +0300

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const targetFolderName = "files";
+const sourceFileName = "archive.gz";
+const targetFileName = "fileToCompress.txt";
 const decompress = async () => {
-    // Write your code here 
+  const sourceFilePath = path.join(__dirname, targetFolderName, sourceFileName);
+  const targetFilePath = path.join(__dirname, targetFolderName, targetFileName);
+
+  const gunzip = createGunzip();
+  const source = fs.createReadStream(sourceFilePath);
+  const destination = fs.createWriteStream(targetFilePath);
+
+  source
+    .pipe(gunzip)
+    .pipe(destination)
+    .on("error", (err) => console.error(err));
 };
 
 await decompress();
