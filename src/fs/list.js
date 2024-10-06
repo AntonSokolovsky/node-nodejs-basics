c2fc54d9e8f8bea4cf6df44c5b7ac4a719d3fa0d
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-06 17:59:41 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const __dirname = import.meta.dirname;
+
+const customError = "FS operation failed";
+const targetFolderName = "files";
 const list = async () => {
-    // Write your code here 
+  const targetFolderPath = path.join(__dirname, targetFolderName);
+
+  try {
+    await fs.access(targetFolderPath);
+    const files = await fs.readdir(targetFolderPath);
+    console.log(files);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error(customError);
+    } else {
+      throw err;
+    }
+  }
 };
 
 await list();
