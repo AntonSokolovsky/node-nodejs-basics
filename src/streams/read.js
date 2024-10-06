6a72d37d964106179c1c1663fba7ecca4fd66683
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-06 22:10:10 +0300

    feat: implement stream read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const targetFolderName = "files";
+const targetFileName = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const targetFilePath = path.join(__dirname, targetFolderName, targetFileName);
+  const stream = fs.createReadStream(targetFilePath, "utf-8");
+
+  let data = "";
+
+  stream.on("data", (chunk) => (data += chunk));
+  stream.on("end", () => console.log(data));
+  stream.on("error", (error) => console.log("Error!", error.message));
 };
 
 await read();
