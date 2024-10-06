acd8c56c2148edc2388c4845559bf16b2e3c229f
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-06 16:39:53 +0300

    feat: implement rename wrong file function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const __dirname = import.meta.dirname;
+
+const customError = "FS operation failed";
+const wrongFileName = "wrongFilename.txt";
+const properFilename = "properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+  const folderPath = path.join(__dirname, "files");
+  const wrongFilePath = path.join(folderPath, wrongFileName);
+  const properFilePath = path.join(folderPath, properFilename);
+
+  try {
+    await fs.access(wrongFilePath);
+    try {
+      await fs.access(properFilePath);
+      throw new Error(customError);
+    } catch (error) {
+      if (error.code !== "ENOENT") throw error;
+    }
+    await fs.rename(wrongFilePath, properFilePath);
+  } catch (error) {
+    throw new Error(customError);
+  }
 };
 
 await rename();
