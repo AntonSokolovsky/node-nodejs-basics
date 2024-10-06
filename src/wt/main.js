d15542eec82167b7452dfb4b138a89106ac2d6ba
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-07 02:29:32 +0300

    feat: implement wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,45 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerFile = "worker.js";
+const startNumber = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workerPath = path.join(__dirname, workerFile);
+  const numberOfCPU = os.cpus().length;
+  const workers = [];
+  const result = new Array(numberOfCPU);
+
+  const createWorker = (index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(workerPath);
+      workers.push(worker);
+
+      worker.postMessage(index + startNumber);
+
+      worker.on("message", (message) => {
+        result[index] = message;
+        resolve();
+      });
+
+      worker.on("error", () => {
+        result[index] = { status: "error", data: null };
+        resolve();
+      });
+    });
+  };
+
+  const workerPromises = Array.from({ length: numberOfCPU }, (_, index) =>
+    createWorker(index)
+  );
+
+  await Promise.all(workerPromises);
+  console.log(result);
 };
 
 await performCalculations();
